8750a3f1a3d1c91da92aad8430f17072bfee26d0
tahajahani <taha7900@gmail.com>
2022-03-07 16:38:32 +0330

    link added to bars

--- shora-frontend/src/Components/Calendar/Bar.js
+++ shora-frontend/src/Components/Calendar/Bar.js
@@ -1,11 +1,27 @@
 import * as React from 'react'
 import { Paper, Tooltip, Typography } from '@mui/material'
 
-function Bar({ data, row, offset, length, width = 36, color, columnWidth =
+function Bar({ title, subtitle, link, row, offset, length, width = 36, color, columnWidth =
     64 }) {
     let right = (row * columnWidth) - (columnWidth - width) / 2 + 4
+
+    const openTab = () => {
+        if (link)
+            window.open(link, '_blank')
+    }
     return (
-        <Tooltip title={<Typography>{data}</Typography>} arrow>
+        <Tooltip
+            title={
+                <div>
+                    <Typography sx={{textAlign: 'center'}}>
+                        {title}
+                    </Typography>
+                    <Typography variant='caption'>
+                        {subtitle}
+                    </Typography>
+                </div>
+            }
+            arrow>
             <div style={{
                 width: width,
                 height: length,
@@ -14,9 +30,10 @@ function Bar({ data, row, offset, length, width = 36, color, columnWidth =
                 display: 'block',
                 top: offset,
                 right: right,
+                cursor: 'pointer',
                 // boxShadow: '2px 2px 4px #888888',
                 borderRadius: '4px',
-            }}>
+            }} onClick={openTab}>
 
             </div>
         </Tooltip>
